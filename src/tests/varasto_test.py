5c51ac9405e07c08f38fa6ea8f52cfcecc176235
Joonas Ahovalli <9369455+joonas-a@users.noreply.github.com>
2022-11-03 21:47:27 +0200

    varasto-testikattavuus 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,44 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_ylitaytto_ei_mahdollinen(self):
+        self.varasto.lisaa_varastoon(20)
+        
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_negatiivinen_ottaminen_ei_mahdollinen(self):
+
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-1), 0.0)
+
+    def test_liian_suuri_maara_ottaa_kaiken_mitä_on(self):
+        self.varasto.ota_varastosta(10)
+
+        self.assertAlmostEqual(self.varasto.ota_varastosta(100), 0)
+
+    def test_string_tulostuu_oikein(self):
+        teksti = self.varasto.__str__()
+
+        self.assertAlmostEqual("saldo = 0, vielä tilaa 10", teksti)
+
+class TestVarasto2(unittest.TestCase):
+    def setUp(self):
+        self.varasto = Varasto(-10, -10)
+    
+    def test_konstruktori_korjaa_virheellisen_tilavuuden(self):
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+
+    def test_konstruktori_korjaa_virheellisen_saldon(self):
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_negatiivinen_lisaaminen_ei_mahdollinen(self):
+        lisays = self.varasto.lisaa_varastoon(-1)
+
+        self.assertEqual(lisays, None)
+
+class TestVarasto3(unittest.TestCase):
+    def setUp(self):
+        self.varasto = Varasto(5, 10)
+
+    def test_suurempi_saldo_kuin_tilavuus_toimii_oikein(self):
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
